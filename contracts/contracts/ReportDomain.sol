ea434c3a59f67d970869caf8b703970ac4e3c5e6
dinesh11515 <dineshaitham2@gmail.com>
2022-09-13 21:42:45 +0530

    added report contract

--- contracts/contracts/ReportDomain.sol
+++ contracts/contracts/ReportDomain.sol
@@ -1,6 +1,5 @@
 pragma solidity 0.8.12;
 
-import "@tableland/evm/contracts/ITablelandTables.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
 import "hardhat/console.sol";
@@ -10,86 +9,65 @@ import "./Token.sol";
 contract ReportDomain is OwnableUpgradeable {
     string ACCEPTED;
     string REJECTED;
-    
-    ITablelandTables private _tableland;
-
-    string public metadataTable;
-    uint256 private _metadataTableId;
-    string private _tablePrefix;
-    uint256 _tableCurrentID;
 
     address governanceBadgeNFT;
     address tokenContract;
+    address treasuryContract;
 
     uint public stakingAmount;
     uint public reward;
     uint public lockedAmount;
+    uint public reportID;
 
     struct DomainClaim {
         bool isScamClaim;
         bool isLegitClaim;
+        address scamReporter;
+        address legitReporter;
+        uint reportIDforScam;
+        uint reportIDforLegit;
     }
 
     struct ReportInfo {
-        address reporter;
-        bool exists;
+        uint id;
         uint stake;
+        uint createdon;
+        uint updatedon;
+        bool exists;
         bool isOpen;
+        bool isScam;
+        string domain;
+        string evidences;
+        string comments;
+        string status;
+        string validatorComments;
+        address reporter;
+        address validator;
     }
 
     mapping(string => DomainClaim) reportsByDomain;
     mapping(uint => ReportInfo) reportsByID;
+    mapping(address => ReportInfo[]) reportsByReporter;
 
 
-    function initialize(address _governanceBadgeNFT, address _tokenContract, address _tablelandRegistry) initializer public {
+    function initialize(address _governanceBadgeNFT, address _tokenContract,address _treasuryContract) initializer public {
         __Ownable_init();
         governanceBadgeNFT = _governanceBadgeNFT;
         tokenContract = _tokenContract;
-        _tableland = ITablelandTables(_tablelandRegistry);
-        _tablePrefix = "domainreports";
-        _tableCurrentID = 0;
+        treasuryContract = _treasuryContract;
         reward = 10 ether;
         stakingAmount = 0.5 ether;
+        reportID = 0;
 
         ACCEPTED = "ACCEPTED";
         REJECTED = "REJECTED";
-
-        string memory createTableCmd = " (id int, domain text, stake text, isScam int, evidences text, comments text, status text, account text, validator text, validatorComments text, createdon text, updatedon text, primary key (id));";
-
-        /*
-        * Stores the unique ID for the newly created table.
-        */
-        _metadataTableId = _tableland.createTable(
-            address(this),
-            string.concat(
-                "CREATE TABLE ",
-                _tablePrefix,
-                "_",
-                StringsUpgradeable.toString(block.chainid),
-                createTableCmd
-            )
-        );
-
-
-        /*
-        * Stores the full tablename for the new table. 
-        * {prefix}_{chainid}_{tableid}
-        */
-        metadataTable = string.concat(
-            _tablePrefix,
-            "_",
-            StringsUpgradeable.toString(block.chainid),
-            "_",
-            StringsUpgradeable.toString(_metadataTableId)
-        );
-
     }
 
     function setReward(uint _reward) public onlyOwner {
         reward = _reward;
     }
 
-    function isReported(string calldata domain, bool isScam) public returns(bool) {
+    function isReported(string calldata domain, bool isScam) public view returns(bool) {
         if(isScam) {
             return reportsByDomain[domain].isScamClaim;
         } else {
@@ -97,29 +75,21 @@ contract ReportDomain is OwnableUpgradeable {
         }
     }
 
-    // function getAllOpenReports() public {
-    //     string memory cmd = string.concat(
-    //             "SELECT * FROM ",
-    //             metadataTable,
-    //             " WHERE status='OPEN'"
-    //     );
-    //     _tableland.runSQL(
-    //         address(this),
-    //         _metadataTableId,
-    //         cmd
-    //     );
-    // }
-
     /*
     * Report a domain
     * params: domain, isScam, evidences, comments
     * checks if a report already exists
     */
     function report(string calldata domain, bool isScam, string[] calldata evidenceHashes, string calldata comments) public payable returns(uint) {
-        if(isScam)
+        if(isScam){
             require(!reportsByDomain[domain].isScamClaim, "Is Scam report already filed");
-        else
+            require(reportsByDomain[domain].legitReporter != msg.sender, "You already reported it as legit");
+        }
+        else{
             require(!reportsByDomain[domain].isLegitClaim, "Is Legit report already filed");
+            require(reportsByDomain[domain].scamReporter != msg.sender, "You already reported it as scam");
+        }
+
 
         require(msg.value == stakingAmount, "Incorrect stake amount sent");
         lockedAmount += stakingAmount;
@@ -141,46 +111,39 @@ contract ReportDomain is OwnableUpgradeable {
             }
         }
 
-        uint isScamInt = isScam ? 1 : 0;
         uint timestamp = block.timestamp;
-        string memory cmd = string.concat(
-                "INSERT INTO ",
-                metadataTable,
-                " (id, domain, stake, isScam, evidences, comments, status, account, validator, validatorComments, createdon, updatedon) VALUES (",
-                StringsUpgradeable.toString(_tableCurrentID),
-                ", '", domain,
-                "', '", StringsUpgradeable.toString(stakingAmount),
-                "', ", StringsUpgradeable.toString(isScamInt),
-                ", '", evidenceHashesSerialised,
-                "', '", comments,
-                "', 'OPEN'",
-                ", '", StringsUpgradeable.toHexString(uint160(msg.sender), 20),
-                "', 'pending', 'pending",
-                "', '", StringsUpgradeable.toString(timestamp),
-                "', '", StringsUpgradeable.toString(timestamp),
-                "')"
-        );
-        console.log(cmd);
-        _tableland.runSQL(
-            address(this),
-            _metadataTableId,
-            cmd
-        );
-
+        
+        reportID = reportID + 1;
         if(isScam) {
             reportsByDomain[domain].isScamClaim = true;
+            reportsByDomain[domain].scamReporter = msg.sender;
+            reportsByDomain[domain].reportIDforScam = reportID;
         } else {
             reportsByDomain[domain].isLegitClaim = true;
+            reportsByDomain[domain].legitReporter = msg.sender;
+            reportsByDomain[domain].reportIDforLegit = reportID;
         }
-        ReportInfo storage _reportInfo = reportsByID[_tableCurrentID];
-        _reportInfo.exists = true;
-        _reportInfo.reporter = msg.sender;
-        _reportInfo.stake = stakingAmount;
-        _reportInfo.isOpen = true;
 
 
-        uint reportID = _tableCurrentID;
-        _tableCurrentID += 1;
+        ReportInfo memory reportInfo = ReportInfo(
+            reportID,
+            stakingAmount,
+            timestamp,
+            timestamp,
+            true,
+            true,
+            isScam,
+            domain,
+            evidenceHashesSerialised,
+            comments,
+            "",
+            "",
+            msg.sender,
+            address(0)
+        );
+
+        reportsByID[reportID] = reportInfo;
+        
         return reportID;
     }
 
@@ -188,46 +151,41 @@ contract ReportDomain is OwnableUpgradeable {
     * Governance Badge NFT holders can validate the claims
     * params: reportId, isAccepted, comments
     */
-    function validate(uint reportId, bool isAccepted, string calldata comments) public {
+    function validate(uint _reportId, bool isAccepted, string calldata comments) public {
         GovernanceBadgeERC1155 _gov = GovernanceBadgeERC1155(governanceBadgeNFT);
         uint256 bal = _gov.balanceOf(msg.sender, _gov.VALIDATOR_NFT());
         require(bal > 0, "Only governance member can validate");
-        require(reportsByID[reportId].exists, "Case doesnt exist");
-        require(reportsByID[reportId].isOpen, "Case already validated");
-
+        require(reportsByID[_reportId].exists, "Case doesnt exist");
+        require(reportsByID[_reportId].isOpen, "Case already validated");
+        if(reportsByID[_reportId].isScam) {
+            require(_reportId < reportsByDomain[reportsByID[_reportId].domain].reportIDforLegit, "First close the legit file");
+        } else {
+            require(_reportId < reportsByDomain[reportsByID[_reportId].domain].reportIDforScam, "First close the scam file");
+        }
         string memory status = ACCEPTED;
         if(!isAccepted)
             status = REJECTED;
 
         uint timestamp = block.timestamp;
-        string memory cmd = string.concat(
-                "UPDATE ",
-                metadataTable,
-                " SET ",
-                "status='",
-                status,
-                "', validatorComments='",
-                comments,
-                "', validator='",
-                StringsUpgradeable.toHexString(uint160(msg.sender), 20),
-                "', updatedon='",
-                StringsUpgradeable.toString(timestamp),
-                "' WHERE id=",
-                StringsUpgradeable.toString(reportId)
-        );
-        console.log(cmd);
-        _tableland.runSQL(
-            address(this),
-            _metadataTableId,
-            cmd
-        );
-        reportsByID[reportId].isOpen = false;
+        reportsByID[_reportId].status = status;
+        reportsByID[_reportId].validatorComments = comments;
+        reportsByID[_reportId].validator = msg.sender;
+        reportsByID[_reportId].updatedon = timestamp;
+        reportsByID[_reportId].isOpen = false;
         if(compareStrings(status, ACCEPTED)) {
-            payable(reportsByID[reportId].reporter).transfer(reportsByID[reportId].stake);
+            payable(reportsByID[_reportId].reporter).transfer(reportsByID[_reportId].stake);
             Token token = Token(tokenContract);
-            token.mint(reportsByID[reportId].reporter, reward);
+            token.mint(reportsByID[_reportId].reporter, reward);
         }
-        lockedAmount -= reportsByID[reportId].stake;
+        else{
+            payable(treasuryContract).transfer(reportsByID[_reportId].stake);
+        }
+
+        lockedAmount -= reportsByID[_reportId].stake;
+        reportsByDomain[reportsByID[_reportId].domain].isScamClaim = false;
+        reportsByDomain[reportsByID[_reportId].domain].isLegitClaim = false;
+        reportsByDomain[reportsByID[_reportId].domain].scamReporter = address(0);
+        reportsByDomain[reportsByID[_reportId].domain].legitReporter = address(0);
     }
 
     function compareStrings(string memory a, string memory b) internal view returns (bool) {
