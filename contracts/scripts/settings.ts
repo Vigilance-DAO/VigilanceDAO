d948c4ef831a9cce526b31cd88a397f1a8b74a9f
Venkat Kunisetty <teja.kunisetty@gmail.com>
2022-10-28 09:02:30 +0530

    new contracts deployed

---
+++ contracts/scripts/settings.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+async function run() {
+    let result = await ethers.getSigners()
+    console.log(result[0].address)
+    async function setStakingAmount() {
+        let amount = 0.01
+
+        let amountWei = ethers.utils.parseEther(amount + '')
+        console.log(amountWei.toString())
+
+        let reportContractAddr = '0x68Db62ADCaADdb21cB000841f1F347A6d8bEED9b'
+        let reportContract = await ethers.getContractAt('ReportDomain', reportContractAddr, result[0])
+        const tx = await reportContract.setStakingAmount(amountWei.toString())
+        console.log(tx.hash)
+        await tx.wait()
+    }
+
+
+    setStakingAmount()
+}
+
+run()
