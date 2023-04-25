6d070d0b4da04f973875f3479ac7166187cba446
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2023-04-25 22:21:42 +0530

    update alert component

--- chrome-extension/src/prebuild-components/alert.tsx
+++ chrome-extension/src/prebuild-components/alert.tsx
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 
 const componentStyles: Record<string, React.CSSProperties> = {
 	internetVigilanceBackdrop: {
-		backgroundColor: "rgb(0 0 0 / 45%)",
 		width: "100%",
 		minHeight: "100vh",
 		position: "absolute",
@@ -12,19 +11,68 @@ const componentStyles: Record<string, React.CSSProperties> = {
 		lineHeight: "20px",
 		zIndex: 1000000000000000,
 	},
-	heading: { margin: "10px 0", fontSize: 20 },
+	header: {
+		display: "grid",
+		gridTemplateColumns: "auto 70px",
+		gridTemplateRows: "repeat(3, auto)",
+		gridTemplateAreas: `
+			'status-message status-image'
+			'status-description status-image'
+			'credit .'
+		`,
+	},
+	heading: { margin: "10px 0", fontSize: 20, gridArea: "status-message" },
+	statusDescription: {
+		gridArea: "status-description",
+		marginTop: 0,
+		marginBottom: "8px",
+	},
+	credits: { gridArea: "credit", opacity: "0.5", fontSize: "0.8rem" },
+	statusImage: {
+		gridArea: "status-image",
+	},
+	domainInfoContainer: {
+		display: "flex",
+		gap: "5px",
+		flexDirection: "column",
+		margin: "10px 0",
+	},
+	buttonContainer: {
+		display: "flex",
+		gap: "6px",
+		justifyItems: "flex-end",
+		margin: "15px 0 6px",
+	},
+	buttonNormal: {
+		border: "1px solid white",
+		background: "none",
+		color: "white",
+		borderRadius: "3px",
+		padding: "5px 10px",
+		fontWeight: "600",
+		opacity: "0.5",
+	},
+	buttonSpecial: {
+		marginRight: "auto",
+		border: "#D9D9D9",
+		background: "none",
+		color: "#0F0F0F",
+		backgroundColor: "#D9D9D9",
+		borderRadius: "3px",
+		padding: "5px 10px",
+		fontWeight: "600",
+	},
 	innerDiv: {
 		width: 350,
-		border: "10px solid #0b182c",
-		padding: 10,
+		padding: "10px 14px",
 		borderRadius: 10,
-		backgroundColor: "#f3f6fb",
 		fontWeight: "bold",
 		position: "fixed",
 		top: 10,
 		right: 10,
-		color: "black",
+		color: "hsla(0, 0%, 82%, 1)",
 		fontSize: 16,
+		backgroundColor: "hsla(0, 0%, 20%, 1)",
 	},
 	closeInternetVigilance: {
 		background: "#5e542d",
@@ -81,34 +129,37 @@ export default function Alert() {
 				id="internetVigilanceBackdrop"
 			>
 				<div style={componentStyles.innerDiv}>
-					<h1 style={componentStyles.heading}>Internet Vigilance</h1>
-					<hr />
-					<p>
-						Domain: <span id="domainName">Loading...</span>
-					</p>
-					<p>
-						Registered on: <span id="domainRegDate">Loading...</span>
-					</p>
-					<p>
-						Warning: This is a newly registered domain. While most newly built
-						websites are safe, a few may be created for fraudulent purposes.
-					</p>
-					<p>
-						Please do necessary research before perfoming any financial
-						transactions or entering your passwords.
-					</p>
-					<button
-						id="closeInternetVigilance"
-						style={componentStyles.closeInternetVigilance}
-					>
-						Close
-					</button>
-					<button
-						id="closeInternetVigilanceWithNoMoreShow"
-						style={componentStyles.closeInternetVigilanceWithNoMoreShow}
-					>
-						Do not show for this website again.
-					</button>
+					<div style={componentStyles.header}>
+						<h1 style={componentStyles.heading}>Likely Dangerous website</h1>
+						<p style={componentStyles.statusDescription}>
+							We have reports that this could be a fraudulent website
+						</p>
+						<span style={componentStyles.credits}>
+							Powered by Vigilance DAO
+						</span>
+						<img
+							style={componentStyles.statusImage}
+							src="./../images/dangerous.png"
+							id="status"
+						/>
+					</div>
+
+					<div style={componentStyles.domainInfoContainer}>
+						<span>
+							Category: <span id="domainName">Loading...</span>
+						</span>
+						<span>
+							Registered on: <span id="domainRegDate">Loading...</span>
+						</span>
+					</div>
+
+					<div style={componentStyles.buttonContainer}>
+						<button style={componentStyles.buttonSpecial}>Close</button>
+						<button style={componentStyles.buttonNormal}>Hide</button>
+						<button style={componentStyles.buttonNormal}>
+							Don't show again
+						</button>
+					</div>
 				</div>
 			</div>
 		</Fragment>
