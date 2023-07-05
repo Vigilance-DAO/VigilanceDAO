007975abe62625b2394fa923e4a156fabd6baa9d
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2023-07-05 13:36:58 +0530

    add financialAlertDialog in inject.js

---
+++ chrome-extension/src/inject.d.ts
@@ -0,0 +1,45 @@
+interface ETH_SendTransactionRequestParamsItem {
+	gas: string;
+	value: string;
+	from: string;
+	to: string;
+	data: string;
+}
+
+interface ETH_SendTransactionRequest {
+	method: "eth_sendTransaction";
+	params: ETH_SendTransactionRequestParamsItem[];
+}
+
+interface MetaMaskRequest {
+	method: string;
+}
+
+interface ContractInfo {
+	userCount24hours: number;
+	userCount30days: number;
+	creationDate: string;
+	name: string;
+}
+
+interface BasicContractInfo {
+	address: string;
+	chain_id: string;
+}
+
+/**  
+ * Example ETH_SendTransactionRequest
+ * {
+    "method": "eth_sendTransaction",
+    "params": [
+        {
+            "gas": "0x1141c",
+            "value": "0x3782dace9d900000",
+            "from": "0x49df7fdcb70b24aead2b28ac1813582b99279304",
+            "to": "0x2ef4a574b72e1f555185afa8a09c6d1a8ac4025c",
+            "data": "0xa5e5657100000000000000000000000021804205c744dd98fbc87898704564d5094bb16700000000000000000000000049df7fdcb70b24aead2b28ac1813582b992793040000000000000000000000000000000000000000000000000000000000000038"
+        }
+    ]
+}
+ * 
+ */
--- chrome-extension/src/inject.js
+++ chrome-extension/src/inject.js
@@ -1,19 +1,234 @@
-const mixpanel = require('mixpanel-browser');
+// @ts-check
+const mixpanel = require("mixpanel-browser");
 const { MIXPANEL_PROJECT_ID } = require("../privateenv");
+const { getFonts } = require("./fonts");
 
-(function() {
-    const metamaskRequest = window.ethereum.request;
-    window.ethereum.request = async (params) => {
-    if(params.method === "eth_sendTransaction") {
-        const { to, from, value, data } = params.params[0];
-        mixpanel.init(MIXPANEL_PROJECT_ID, {debug: true});
-        mixpanel.identify(from);
-        mixpanel.track("Transaction", { 
-            "toAddress" : to,
-            "value" : value,
-            "data" : data
-        });
-    }  
-    return await metamaskRequest({ ...params })
+const ContractInfoAPIURL =
+	"https://8md2nmtej9.execute-api.ap-northeast-1.amazonaws.com/contract-info";
+
+/**
+ * @param {MetaMaskRequest} params
+ * @returns {params is ETH_SendTransactionRequest}
+ */
+function isSendTransactionRequest(params) {
+	return params.method == "eth_sendTransaction";
 }
+
+/**
+ * @param {BasicContractInfo} basicInfo
+ * @returns {Promise<ContractInfo | null>}
+ */
+function fetchContractInfo(basicInfo) {
+	return fetch(ContractInfoAPIURL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(basicInfo),
+	})
+		.then((response) => response.json())
+		.then(
+			/**
+			 * @param {ContractInfo} jsonData
+			 */
+			(jsonData) => {
+				console.log(jsonData);
+				return jsonData;
+			}
+		)
+		.catch((error) => {
+			console.error(error);
+			return null;
+		});
+}
+
+/**
+ * @type {<E extends Element = Element>(shadowRoot: ShadowRoot, selector: string) => E}
+ */
+function querySelector(shadowRoot, selector) {
+	const element = shadowRoot.querySelector(selector);
+
+	if (element == null) {
+		throw new Error(`No element found for ${selector}`);
+	}
+
+	// @ts-expect-error
+	return element;
+}
+
+const financialAlertDialog = document.createElement("dialog");
+let financialAlertDialogInnerHtml = "";
+
+/**
+ * @typedef FinancialAlertInfo
+ * @prop {string} contract
+ * @prop {string} createdOn
+ * @prop {"High"} drainedAccountsValue
+ * @prop {number} transactionsIn24hours
+ * @prop {number} transactionsIn30days
+ * @prop {() => void} cancelButtonClickListener
+ * @prop {() => void} proceedButtonClickListener
+ *
+ * @param {FinancialAlertInfo} alertInfo
+ */
+async function createFinancialAlertDialog(alertInfo) {
+	console.log("createFinancialAlertDialog", alertInfo);
+	if (alertInfo == undefined) {
+		console.error(
+			"createFinancialAlertDialog: alertInfo parameter is required"
+		);
+		return;
+	}
+
+	const INNER_BORDER_RADIUS = 10;
+	const BORDER_WIDTH = 10;
+	const OUTER_BORDER_RADIUS = INNER_BORDER_RADIUS + BORDER_WIDTH;
+
+	if (financialAlertDialogInnerHtml == "") {
+		financialAlertDialog.style.borderRadius =
+			OUTER_BORDER_RADIUS.toString().concat("px");
+		financialAlertDialog.style.zIndex = "10000";
+		financialAlertDialog.style.margin = "auto clamp(10px, 3vw, 30px) 25px auto";
+		financialAlertDialog.style.height = "fit-content";
+		financialAlertDialog.style.border = "none";
+		financialAlertDialog.style.padding = "0";
+		financialAlertDialog.style.background =
+			"linear-gradient(to bottom, hsl(265, 100%, 40%), hsl(265, 94%, 19%))";
+
+		/**
+		 * @type {string[]}
+		 */
+		const innerHTMLParts = new Array(3).fill("");
+		// part 0 -> fonts
+		// part 1 -> css variables
+		// part 2 -> financial-alert component content
+
+		innerHTMLParts[0] = `<style>${getFonts()}</style>`;
+		innerHTMLParts[1] = `<style>:host { --border: ${BORDER_WIDTH}px; --inner-border-radius: ${INNER_BORDER_RADIUS}px; }</style>`;
+		innerHTMLParts[2] = await fetch(
+			chrome.runtime.getURL("static/financial-alert.html")
+		)
+			.then((response) => response.text())
+			.catch((e) => {
+				console.error("Error while loading html from financial-alert.html", e);
+				return "";
+			});
+
+		financialAlertDialogInnerHtml = innerHTMLParts.join("");
+	}
+
+	financialAlertDialog.innerHTML = "";
+	const div = document.createElement("div");
+	const shadowRoot = div.attachShadow({ mode: "closed" });
+	financialAlertDialog.appendChild(div);
+
+	shadowRoot.innerHTML = financialAlertDialogInnerHtml;
+
+	const select = querySelector.bind(null, shadowRoot);
+
+	const contractInfoElement = select(".contract-info");
+	const contractCreatedOnElement = select(".contract-created-on");
+	const transactionsIn24hoursElement = select(".transactions-in-day");
+	const transactionsIn30daysElement = select(".transactions-in-month");
+	const drainedAccountsValueElement = select(".drained-info .value");
+
+	const proceedButton = select("#proceed-btn");
+	const closeButton = select("#close-btn");
+
+	let formattedTransactionsIn30days = alertInfo.transactionsIn30days.toString();
+	if (alertInfo.transactionsIn30days >= 1000) {
+		formattedTransactionsIn30days = Math.floor(
+			alertInfo.transactionsIn30days / 1000
+		)
+			.toString()
+			.concat("K");
+	}
+
+	contractInfoElement.innerHTML = alertInfo.contract;
+	contractCreatedOnElement.innerHTML = alertInfo.createdOn;
+	transactionsIn24hoursElement.innerHTML =
+		alertInfo.transactionsIn24hours.toString();
+	transactionsIn30daysElement.innerHTML = formattedTransactionsIn30days;
+	drainedAccountsValueElement.innerHTML = alertInfo.drainedAccountsValue;
+	proceedButton.addEventListener("click", alertInfo.proceedButtonClickListener);
+	closeButton.addEventListener("click", alertInfo.cancelButtonClickListener);
+
+	financialAlertDialog.className = "____vigilance-dao-dialog____";
+	document.body.appendChild(financialAlertDialog);
+	financialAlertDialog.show();
+}
+
+(function () {
+	if (window.ethereum == undefined) {
+		console.warn("Metamask extension is not installed");
+		return;
+	}
+
+	// @ts-expect-error
+	const metamaskRequest = window.ethereum.request;
+	/**
+	 * @param {MetaMaskRequest} params
+	 */
+	// @ts-expect-error
+	window.ethereum.request = (params) => {
+		return /** @type {Promise<void>} */ (
+			new Promise(async (continueRequest, reject) => {
+				console.log("INJECTED", params);
+				console.log("isSendTransactionRequest", params);
+
+				if (window.ethereum == undefined || !isSendTransactionRequest(params)) {
+					continueRequest();
+					return;
+				}
+
+				// @ts-expect-error
+				const chainId = window.ethereum.networkVersion;
+				console.log("chainId", chainId);
+
+				const { to, from, value, data } = params.params[0];
+				mixpanel.init(MIXPANEL_PROJECT_ID, { debug: true });
+				mixpanel.identify(from);
+				mixpanel.track("Transaction", {
+					"toAddress": to,
+					"value": value,
+					"data": data,
+					chainId,
+				});
+
+				const contractInfo = await fetchContractInfo({
+					address: to,
+					chain_id: chainId,
+				});
+				console.log("contractInfo", contractInfo);
+
+				if (contractInfo == null) {
+					// TODO decide what to do at this point
+					continueRequest();
+					return;
+				}
+				createFinancialAlertDialog({
+					createdOn: contractInfo.creationDate,
+					contract: contractInfo.name,
+					transactionsIn24hours: contractInfo.userCount24hours,
+					transactionsIn30days: contractInfo.userCount30days,
+					proceedButtonClickListener: () => {
+						console.log("proceed btn clicked");
+						financialAlertDialog.close();
+						// continueRequest();
+						// TODO FOR TESTING ONLY
+						reject();
+					},
+					cancelButtonClickListener: () => {
+						financialAlertDialog.close();
+						reject(new Error("Transaction cancelled by user."));
+					},
+					// TODO
+					drainedAccountsValue: "High",
+				});
+			})
+		).then(() => {
+			console.log("will complete transaction now");
+			return metamaskRequest({ ...params });
+		});
+	};
 })();
