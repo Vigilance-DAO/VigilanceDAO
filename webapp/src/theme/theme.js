8c368b7ba21ae88a8a31e01f323a67270e167bdd
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2023-08-07 20:11:45 +0530

    fix: theming issue

--- webapp/src/theme/theme.js
+++ webapp/src/theme/theme.js
@@ -1,7 +1,15 @@
 import { extendTheme, theme as base } from "@chakra-ui/react";
 
 export const theme = extendTheme({
-  fonts: {
-    Jost: `Jost, ${base.fonts.body}`,
-  },
+	fonts: {
+		Jost: `Jost, ${base.fonts.body}`,
+	},
+	styles: {
+		global: {
+			body: {
+				backgroundColor: "#04051C",
+				color: "white"
+			},
+		},
+	},
 });
