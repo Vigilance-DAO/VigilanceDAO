b488c4854d0d8e4f15d341d403261c1d4564cb00
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2023-08-09 23:49:19 +0530

    chore: extract domainInfo route handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,14 @@
 import express, { json } from 'express';
 import helmet from 'helmet';
 import { PoolClient } from 'pg';
-import pool, { poolContracts } from './db';
+import { poolContracts } from './db';
 import {ethers}  from 'ethers';
 import fetch from 'node-fetch';
-import { BasicDomainInfo, DomainInfo, DomainScamInfo } from "../../important-types";
 import client from './discord-client';
 import { ContractReport__Unknown, ContractReport, ContractReport_FraudType } from './types';
-const whois = require('whois-json');
+
+import domainInfo from './routes/domainInfo';
+
 const cors = require('cors');
 const { sendMessage } = require('./telegramBot');
 
@@ -18,27 +19,7 @@ app.use(
 	cors(),
 );
 
-app.post('/domain-info', async (req, res) => {
-	// await captureWebsite.file('https://cryptnesis.com/', 'screenshot.png');
-
-	let domain = req.body.domain
-	console.log('domain', domain)
-	if (!domain) {
-		res.status(400).send({})
-		return;
-	}
-
-	try {
-		let client = await pool.connect()
-		let output = await getDomainInfo(client, domain)
-		client.release()
-		res.json(output);
-	} catch (err) {
-		console.log('get domain details', err)
-		res.status(500).send({})
-	}
-	return;
-});
+app.post('/domain-info', domainInfo);
 
 app.post("/contract-info", async (req, res) => {
   const address = req.body.address;
@@ -195,80 +176,7 @@ app.use((_, res, _2) => {
 	res.status(404).json({ error: 'NOT FOUND' });
 });
 
-async function getDomainCreatedInfoFromDb(client: PoolClient, domain: string): Promise<BasicDomainInfo | null> {
-	const query = 'SELECT * from domains where domain=$1'
 
-	try {
-		const data = await client.query(query, [domain])
-		if (data.rows.length && data.rows[0].isValid) {
-			return data.rows[0] as BasicDomainInfo;
-		} else {
-			return null;
-		}
-	} catch (err) {
-		console.log('error', err)
-	}
-	return null
-}
-
-async function getDomainScamInfoFromDb(client: PoolClient, domain: string): Promise<DomainScamInfo | null> {
-	const query = 'SELECT "isScam", "fromSourceId", "attackType", "updatedOn" FROM "Edge" WHERE "destinationAddress"=$1';
-
-	try {
-		const data = await client.query(query, [domain]);
-		if (data.rowCount > 0) {
-			return data.rows[0] as DomainScamInfo;
-		} else {
-			return null;
-		}
-	} catch (err) {
-		console.log("error", err);
-	}
-
-	return null;
-}
-
-async function getDomainInfo(client: PoolClient, domain: string): Promise<DomainInfo> {
-	const domainInfo: DomainInfo = {
-		domain,
-	} as DomainInfo;
-
-	const fromDb = await getDomainCreatedInfoFromDb(client, domain)
-	if (fromDb) {
-		console.log('loading from db', domain, fromDb)
-		domainInfo.createdon = fromDb.createdon;
-		domainInfo.updatedon = fromDb.updatedon;
-		domainInfo.recordCreatedOn = fromDb.recordCreatedOn;
-		domainInfo.isValid = fromDb.isValid;
-	} else {
-		const results = await whois(domain);
-		console.log('reading new domain info', domain, results);
-		domainInfo.createdon = new Date(results.creationDate)
-		domainInfo.updatedon = new Date(results.updatedDate)
-		domainInfo.isValid = true;
-
-		let text = 'INSERT INTO domains(domain, createdon, updatedon) VALUES($1, $2, $3)'
-		let values: any = [domain, domainInfo.createdon, domainInfo.updatedon]
-		if (isNaN(domainInfo.createdon as any) || isNaN(domainInfo.updatedon as any)) {
-			text = 'INSERT INTO domains(domain, "isValid") VALUES($1, $2)'
-			values = [domain, false]
-			domainInfo.isValid = false;
-		}
-		try {
-			await client.query(text, values)
-		} catch (err) {
-			console.log('error', err)
-		}
-	}
-	const scamInfoFromDb = await getDomainScamInfoFromDb(client, domain);
-	if (scamInfoFromDb) {
-		domainInfo.scamInfo = scamInfoFromDb;
-	} else {
-		// do nothing
-	}
-
-	return domainInfo;
-}
 
 function getEtherscanEndpoint(chain_id: string) {
 	if (chain_id == Networks.ETHEREUM_MAINNET.valueOf()) {
