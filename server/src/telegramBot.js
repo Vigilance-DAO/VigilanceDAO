2b43523dfa4e2e5bf360fdc028a667af6cd93b34
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2023-08-19 10:17:28 +0530

    chore: enable typing for telegramBot.js

--- server/src/telegramBot.js
+++ server/src/telegramBot.js
@@ -1,4 +1,5 @@
-const TelegramBot = require('node-telegram-bot-api');
+// @ts-check
+const TelegramBot = require("node-telegram-bot-api");
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
@@ -8,38 +9,80 @@ if (!TELEGRAM_CHAT_ID) {
 
 // Create a bot that uses 'polling' to fetch new updates
 
+/**
+ * @type {TelegramBot | null}
+ */
 let botInfo = null;
 if (TELEGRAM_TOKEN) {
-    botInfo = new TelegramBot(TELEGRAM_TOKEN, {polling: false});
+	botInfo = new TelegramBot(TELEGRAM_TOKEN, { polling: false });
 } else {
-    console.warn('No telegram token found. No failure messages will be sent. [Not to worry in dev]')
+	console.warn(
+		"No telegram token found. No failure messages will be sent. [Not to worry in dev]"
+	);
+}
+/**
+ * @typedef Member
+ * @prop {number} id
+ * @prop {string} name
+ */
+
+/**
+ * @type {Member[]}
+ */
+let allowedMembers = [];
+
+if (TELEGRAM_CHAT_ID) {
+	allowedMembers = [{ id: parseInt(TELEGRAM_CHAT_ID), name: "abc" }];
 }
 
-let allowedMembers = [{id: parseInt(TELEGRAM_CHAT_ID), name: 'abc'}]
+/**
+ * @param {string} message
+ * @returns {Promise<void>}
+ */
+export function sendMessage(
+	message,
+	alertType = "infoAlert",
+	options = {},
+	members = allowedMembers,
+	i = 0
+) {
+	return new Promise((resolve, reject) => {
+		let member = members[i];
 
-export function sendMessage(message, alertType='infoAlert', options={}, members = allowedMembers, i=0) {
-    return new Promise((resolve, reject)=> {
-        let member = allowedMembers[i]
-        console.log("Sending messaAGE TO: " + member.id)
-        let bot = botInfo
-        if (!bot) {
-            console.log('No telegram token found')
-            resolve()
-            return
-        }
-        bot.sendMessage(member.id, message, options).then(()=> {
-            console.log('sent', member.id)
-            if((i+1)<allowedMembers.length)
-                sendBoth(message, alertType, options, allowedMembers, i+1).then(()=> {
-                    resolve()
-                }).catch((err)=> {
-                    reject(err)
-                })
-            else
-                resolve()
-        }).catch((err)=> {
-            console.error('message sending failed', i, err);
-            resolve()
-        })
-    })
+		if (member == undefined) {
+			resolve();
+			return;
+		}
+
+		console.log("Sending messaAGE TO: " + member.id);
+		let bot = botInfo;
+		if (!bot) {
+			console.log("No telegram token found");
+			resolve();
+			return;
+		}
+		bot
+			.sendMessage(member.id, message, options)
+			.then(() => {
+				if (member == undefined) return;
+
+				console.log("sent", member.id);
+				if (i + 1 < members.length) {
+					// TODO sendBoth is not defined?
+					// sendBoth(message, alertType, options, allowedMembers, i + 1)
+					// 	.then(() => {
+					// 		resolve();
+					// 	})
+					// 	.catch((err) => {
+					// 		reject(err);
+					// 	});
+				} else {
+					resolve();
+				}
+			})
+			.catch((err) => {
+				console.error("message sending failed", i, err);
+				resolve();
+			});
+	});
 }
